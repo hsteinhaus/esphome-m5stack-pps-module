2c18a53e5944d10316c7f4707a73878ca6b865bd
Holger Steinhaus <hsteinhaus@gmx.de>
2025-04-29 22:18:35 +0200

    number: add optional step attribute

--- components/m5stack_pps/number/__init__.py
+++ components/m5stack_pps/number/__init__.py
@@ -1,7 +1,7 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import number, web_server
-from esphome.const import CONF_ID, CONF_CHANNEL, CONF_VOLTAGE, CONF_CURRENT, DEVICE_CLASS_VOLTAGE, DEVICE_CLASS_CURRENT, ENTITY_CATEGORY_CONFIG
+from esphome.const import CONF_ID, CONF_STEP, CONF_CHANNEL, CONF_VOLTAGE, CONF_CURRENT, DEVICE_CLASS_VOLTAGE, DEVICE_CLASS_CURRENT, ENTITY_CATEGORY_CONFIG
 
 from .. import CONF_M5STACK_PPS_ID, M5STACK_PPS_COMPONENT_SCHEMA, m5stack_pps_ns, M5StackPPSComponent
 
@@ -23,6 +23,7 @@ CONFIG_SCHEMA = cv.All(
       .extend({
         cv.Required(CONF_M5STACK_PPS_ID): cv.use_id(M5StackPPSComponent),
         cv.Required(CONF_CHANNEL): cv.enum(OUTPUT_CHANNELS),
+        cv.Optional(CONF_STEP): cv.float_,
       })
 )
 
@@ -33,11 +34,15 @@ async def to_code(config):
     await cg.register_component(var, config)
     paren = await cg.get_variable(config[CONF_M5STACK_PPS_ID])
 
+    step_config = config.get(CONF_STEP)
+
     if output_channel == CONF_VOLTAGE:
-        await number.register_number(var, config, min_value=0.5, max_value=30, step=0.1)
+        await number.register_number(var, config, min_value=0.5, max_value=30, step=step_config or 0.1)
         cg.add(var.set_parent(paren))
         cg.add(paren.set_output_voltage_number(var))
     else:
-        await number.register_number(var, config, min_value=0, max_value=5, step=0.001)
+        await number.register_number(var, config, min_value=0, max_value=5, step=step_config or 0.001)
         cg.add(var.set_parent(paren))
         cg.add(paren.set_output_current_number(var))
+
+
